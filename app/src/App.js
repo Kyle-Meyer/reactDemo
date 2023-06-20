75e3ad018f0c26fd699b48f0eb3541516c764340
kmeyer <meyerkb@leidos.com>
2023-06-20 13:39:16 -0400

    simple changes

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
 
+import Card from "./Card";
+//the actual function
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hee Hee Hee Haw</h1>
+      <Card/>
     </div>
   );
 }
-
+//export the function
 export default App;
---
+++ app/src/Card.js
@@ -0,0 +1,14 @@
+function Card()
+{
+    return(
+    <div className='card'>
+        <h2>TITLE</h2>
+        <div className='actions'>
+                <button className='btn'>Poop</button>
+                <button>Pee</button>
+        </div>
+    </div>
+    );
+}
+
+export default Card;
